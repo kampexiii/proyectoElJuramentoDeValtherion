27e598580521a40a2f7508d64ee1272cda479a58
Kampexiii <kampexiii@gmail.com>
2026-01-19 16:03:09 +0100

    logo centrado en welcome y traducciones a castellano en auth

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<x-auth-layout>
+    <div class="mb-4 text-sm text-zinc-400">
+        ¿Has olvidado tu contraseña? No hay problema. Dinós tu dirección de correo electrónico y te enviaremos un enlace para restablecerla.
     </div>
 
     <!-- Session Status -->
@@ -11,15 +11,15 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" value="Correo Electrónico" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                Enviar enlace de restablecimiento
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-auth-layout>
