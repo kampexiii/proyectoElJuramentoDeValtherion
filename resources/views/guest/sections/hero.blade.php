906facbab95c59f8b59408fe377ffbc850fbfe12
Kampexiii <kampexiii@gmail.com>
2026-01-26 12:18:26 +0100

    Corrige legibilidad del botón Acceder

--- resources/views/guest/sections/hero.blade.php
+++ resources/views/guest/sections/hero.blade.php
@@ -6,7 +6,7 @@
         <p class="lead hero-subtitle mb-4">Esta sección será el Hero: gancho + botones</p>
         <div class="d-flex justify-content-center gap-3">
             <a href="{{ route('register') }}" class="btn btn-primary btn-lg">Registrarse</a>
-            <a href="{{ route('login') }}" class="btn btn-outline-light btn-lg btn-hero-access">Acceder</a>
+            <a href="{{ route('login') }}" class="btn btn-lg btn-hero-access">Acceder</a>
         </div>
     </div>
 </section>
