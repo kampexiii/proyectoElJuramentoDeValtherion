efa9263c9a8920a688ec6761161c2e31770d087a
Kampexiii <kampexiii@gmail.com>
2026-01-26 12:26:55 +0100

    Aplica estilo marrón oscuro desgastado a entrar_en_la_historia

--- resources/views/guest/sections/entrar_en_la_historia.blade.php
+++ resources/views/guest/sections/entrar_en_la_historia.blade.php
@@ -1,7 +1,7 @@
-<section class="mb-5 text-center bg-transparent p-4">
-    <p class="text-white fst-italic fs-5 mb-4">“Toda historia de ida, tiene un camino de vuelta...”</p>
+<section class="mb-5 text-center bg-transparent p-4 entrar_en_la_historia">
+    <p class="fst-italic fs-5 mb-4">“Toda historia de ida, tiene un camino de vuelta...”</p>
     <div>
-        <a href="#hero" class="text-white text-decoration-none border-bottom border-white pb-1">
+        <a href="#hero" class="text-decoration-none border-bottom pb-1">
             ↑ Volver al Inicio para Realizar el Juramento ↑ 
         </a>
     </div>
