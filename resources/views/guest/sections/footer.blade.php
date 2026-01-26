466005d04ac399a5c70206e44466dca252e7571a
Kampexiii <kampexiii@gmail.com>
2026-01-26 12:33:09 +0100

    Limpia formato del footer y distingue títulos

--- resources/views/guest/sections/footer.blade.php
+++ resources/views/guest/sections/footer.blade.php
@@ -3,12 +3,12 @@
         <div class="row">
             <!-- Logo y descripción -->
             <div class="col-md-4 mb-4">
-                <h5 class="text-warning">El Juramento de Valtherion</h5>
-                <p class="small">Embárcate en una aventura épica donde tus decisiones forjan el destino del reino. Únete a la comunidad y realiza tu juramento.</p>
+                <h5 class="text-warning fw-bold mb-3">El Juramento de Valtherion</h5>
+                <p class="small text-muted">Embárcate en una aventura épica donde tus decisiones forjan el destino del reino. Únete a la comunidad y realiza tu juramento.</p>
             </div>
             <!-- Enlaces rápidos -->
             <div class="col-md-4 mb-4">
-                <h6>Enlaces</h6>
+                <h6 class="text-warning fw-bold mb-3 text-uppercase">Enlaces</h6>
                 <ul class="list-unstyled">
                     <li><a href="{{ route('register') }}" class="text-light text-decoration-none">Registrarse</a></li>
                     <li><a href="{{ route('login') }}" class="text-light text-decoration-none">Iniciar Sesión</a></li>
@@ -21,7 +21,7 @@
             </div>
             <!-- Redes sociales -->
             <div class="col-md-4 mb-4">
-                <h6>Síguenos</h6>
+                <h6 class="text-warning fw-bold mb-3 text-uppercase">Síguenos</h6>
                 <div class="d-flex gap-3">
                     <a href="#" class="text-light" title="Discord"><i class="fab fa-discord fa-lg"></i></a>
                     <a href="#" class="text-light" title="Twitter"><i class="fab fa-twitter fa-lg"></i></a>
@@ -30,10 +30,10 @@
                 </div>
             </div>
         </div>
-        <hr class="my-4">
+        <hr class="my-4 border-secondary">
         <div class="row">
             <div class="col-12 text-center">
-                <small>&copy; {{ date('Y') }} El Juramento de Valtherion. Todos los derechos reservados.</small>
+                <small class="text-muted">&copy; {{ date('Y') }} El Juramento de Valtherion. Todos los derechos reservados.</small>
             </div>
         </div>
     </div>
