a9aa613ff9e095c3c221b517f43abe0bd41463b2
Kampexiii <kampexiii@gmail.com>
2026-01-26 12:36:11 +0100

    Centra elementos del footer y añade borde marrón a enlaces

--- resources/views/guest/sections/footer.blade.php
+++ resources/views/guest/sections/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark text-light py-5 mt-5">
     <div class="container">
-        <div class="row">
+        <div class="row text-center">
             <!-- Logo y descripción -->
             <div class="col-md-4 mb-4">
                 <h5 class="text-warning fw-bold mb-3">El Juramento de Valtherion</h5>
@@ -10,19 +10,19 @@
             <div class="col-md-4 mb-4">
                 <h6 class="text-warning fw-bold mb-3 text-uppercase">Enlaces</h6>
                 <ul class="list-unstyled">
-                    <li><a href="{{ route('register') }}" class="text-light text-decoration-none">Registrarse</a></li>
-                    <li><a href="{{ route('login') }}" class="text-light text-decoration-none">Iniciar Sesión</a></li>
-                    <li><a href="#hero" class="text-light text-decoration-none">Inicio</a></li>
-                    <li><a href="#razas" class="text-light text-decoration-none">Razas</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">Legales</a></li>
-                    <li><a href="https://amazon.com" class="text-light text-decoration-none">Libro</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">Lore</a></li>
+                    <li><a href="{{ route('register') }}" class="text-light text-decoration-none border-bottom border-secondary pb-1">Registrarse</a></li>
+                    <li><a href="{{ route('login') }}" class="text-light text-decoration-none border-bottom border-secondary pb-1">Iniciar Sesión</a></li>
+                    <li><a href="#hero" class="text-light text-decoration-none border-bottom border-secondary pb-1">Inicio</a></li>
+                    <li><a href="#razas" class="text-light text-decoration-none border-bottom border-secondary pb-1">Razas</a></li>
+                    <li><a href="#" class="text-light text-decoration-none border-bottom border-secondary pb-1">Legales</a></li>
+                    <li><a href="https://amazon.com" class="text-light text-decoration-none border-bottom border-secondary pb-1">Libro</a></li>
+                    <li><a href="#" class="text-light text-decoration-none border-bottom border-secondary pb-1">Lore</a></li>
                 </ul>
             </div>
             <!-- Redes sociales -->
             <div class="col-md-4 mb-4">
                 <h6 class="text-warning fw-bold mb-3 text-uppercase">Síguenos</h6>
-                <div class="d-flex gap-3">
+                <div class="d-flex justify-content-center gap-3">
                     <a href="#" class="text-light" title="Discord"><i class="fab fa-discord fa-lg"></i></a>
                     <a href="#" class="text-light" title="Twitter"><i class="fab fa-twitter fa-lg"></i></a>
                     <a href="#" class="text-light" title="Instagram"><i class="fab fa-instagram fa-lg"></i></a>
